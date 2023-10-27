83d3d4d43e17bdf502ac979042c195dd47d4f6e3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-27 03:06:12 +0100

    fix: modify

--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
-const formEl = document.querySelector('#form');
 const copyright = document.querySelector('.footer__copyright-year');
-
-formEl.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const category = document.getElementById('category').value;
-  const content = document.getElementById('content').value;
-
-  console.log({ name, email, category, content });
-
-  document.getElementById('name').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('category').value = '';
-  document.getElementById('content').value = '';
-});
 
 copyright.innerHTML = new Date().getFullYear();
